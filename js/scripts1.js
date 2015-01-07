b9526bf17b50c1af1b2658d7d4725cbb41da543a
Daniel <danielhernandez16@hotmail.com>
2015-01-07 11:36:43 -0500

    Create model & controller and conect with HTML

---
+++ js/scripts1.js
@@ -0,0 +1,10 @@
+// alumnos es el modelo
+
+function AlumnosController($scope){
+	$scope.alumnos=[
+		{nombre:"Juan Blanco", telefono:"1234567890", curso:"Segundo ESO"},
+		{nombre:"Lorena Hernandez", telefono:"3204488428", curso:"Primero ESO"},
+		{nombre:"Daniel Hernandez", telefono:"3116710257", curso:"Segundo ESO"},
+		{nombre:"Jonatan Graterol", telefono:"1234567890", curso:"Tercero ESO"}
+	];
+}
